9ea3236a692ea2692adc19cccb27e27c52b525d3
gohyunjun <64692926+gohyunjun@users.noreply.github.com>
2020-05-03 03:43:56 +0900

    Update pa1.c

--- pa1.c
+++ pa1.c
@@ -18,12 +18,14 @@
 #include <string.h>
 #include <getopt.h>
 #include <ctype.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #include "types.h"
 
-/*====================================================================*/
-/*          ****** DO NOT MODIFY ANYTHING FROM THIS LINE ******       */
-/**/
+ /*====================================================================*/
+ /*          ****** DO NOT MODIFY ANYTHING FROM THIS LINE ******       */
+ /**/
 
 #define MAX_NR_TOKENS	32	/* Maximum length of tokens in a command */
 #define MAX_TOKEN_LEN	128	/* Maximum length of single token */
@@ -36,53 +38,54 @@ static char __prompt[MAX_TOKEN_LEN] = "$";
 
 /**
  *          ****** DO NOT MODIFY ANYTHING UP TO THIS LINE ******      */
-/*====================================================================*/
+ /*====================================================================*/
 
 
-/***********************************************************************
- * parse_command()
- *
- * DESCRIPTION
- *  Parse @command, and put each command token into @tokens[] and the number of
- *  tokes into @nr_tokens. You may use this implemention or your own from PA0.
- *
- *  A command token is defined as a string without any whitespace (i.e., *space*
- *  and *tab* in this programming assignment). For exmaple,
- *   command = "  cp  -pr /home/sslab   /path/to/dest  "
- *
- *  then, nr_tokens = 4, and tokens is
- *    tokens[0] = "cp"
- *    tokens[1] = "-pr"
- *    tokens[2] = "/home/sslab"
- *    tokens[3] = "/path/to/dest"
- *    tokens[>=4] = NULL
- *
- * RETURN VALUE
- *  Return 1 if @nr_tokens > 0
- *  Return 0 otherwise
- */
-int parse_command(char *command, int *nr_tokens, char *tokens[])
+ /***********************************************************************
+  * parse_command()
+  *
+  * DESCRIPTION
+  *  Parse @command, and put each command token into @tokens[] and the number of
+  *  tokes into @nr_tokens. You may use this implemention or your own from PA0.
+  *
+  *  A command token is defined as a string without any whitespace (i.e., *space*
+  *  and *tab* in this programming assignment). For exmaple,
+  *   command = "  cp  -pr /home/sslab   /path/to/dest  "
+  *
+  *  then, nr_tokens = 4, and tokens is
+  *    tokens[0] = "cp"
+  *    tokens[1] = "-pr"
+  *    tokens[2] = "/home/sslab"
+  *    tokens[3] = "/path/to/dest"
+  *    tokens[>=4] = NULL
+  *
+  * RETURN VALUE
+  *  Return 1 if @nr_tokens > 0
+  *  Return 0 otherwise
+  */
+int parse_command(char* command, int* nr_tokens, char* tokens[])
 {
-	char *curr = command;
-	int token_started = false;
-	*nr_tokens = 0;
+    char* curr = command;
+    int token_started = false;
+    *nr_tokens = 0;
 
-	while (*curr != '\0') {  
-		if (isspace(*curr)) {  
-			*curr = '\0';
-			token_started = false;
-		} else {
-			if (!token_started) {
-				tokens[*nr_tokens] = curr;
-				*nr_tokens += 1;
-				token_started = true;
-			}
-		}
+    while (*curr != '\0') {
+        if (isspace(*curr)) {
+            *curr = '\0';
+            token_started = false;
+        }
+        else {
+            if (!token_started) {
+                tokens[*nr_tokens] = curr;
+                *nr_tokens += 1;
+                token_started = true;
+            }
+        }
 
-		curr++;
-	}
+        curr++;
+    }
 
-	return (*nr_tokens > 0);
+    return (*nr_tokens > 0);
 }
 
 
@@ -98,111 +101,149 @@ int parse_command(char *command, int *nr_tokens, char *tokens[])
  *   Return 0 when user inputs "exit"
  *   Return <0 on error
  */
-static int run_command(int nr_tokens, char *tokens[])
+static int run_command(int nr_tokens, char* tokens[])
 {
-	/* This function is all yours. Good luck! */
+    /* This function is all yours. Good luck! */
 
-	if (strncmp(tokens[0], "exit", strlen("exit")) == 0) {
-		return 0;
-	}
-    else if (strncmp(tokens[0], "prompt", strlen("prompt"))) {
-
-        strcpy(__prompt, *tokens[1]);
-
+    if (strncmp(tokens[0], "exit", strlen("exit")) == 0) {
+        return 0;
     }
 
-	/*
-	fork();
-	exec();
-	...
-	*/
+    if (strncmp(tokens[0], "prompt", strlen("prompt"))) {
 
-	return 1;
-}
+        strcpy(__prompt, tokens[1]);
+
+    }
+    else if (strncmp(tokens[0], "cd", strlen("cd"))) {
+
+        strcpy(__prompt, tokens[1]);
+    }
+
+    else if (strncmp(tokens[0], "for", strlen("for"))) {
+
+        char* tokens2[nr_tokens - 2];
+        int num = (int)*tokens[1];
+
+        for (int i = 0; tokens[i]; i++) {
+            tokens2[i] = tokens[i + 2];
+        }
+
+        for (int i = 0; i < num;) {
+            run_command(nr_tokens - 2, tokens2);
+        }
+    }
+    else {
+        pid_t pid = fork();
+
+        if (pid == -1) {
+            return 0;
+        }
+        else if (pid == 0) {
+            if (execvp(tokens[0], tokens) < 0) {
+                fprintf(stdout, "\nCould not execute command..");
+            }
+            return 0;
+        }
+        else {
+            wait(NULL);
+            return 0;
+        }
+
+        /*
+        fork();
+        exec();
+        ...
+        */
 
 
-/***********************************************************************
- * initialize()
- *
- * DESCRIPTION
- *   Call-back function for your own initialization code. It is OK to
- *   leave blank if you don't need any initialization.
- *
- * RETURN VALUE
- *   Return 0 on successful initialization.
- *   Return other value on error, which leads the program to exit.
- */
-static int initialize(int argc, char * const argv[])
-{
-	return 0;
-}
+
+        return 1;
+    }
 
 
-/***********************************************************************
- * finalize()
- *
- * DESCRIPTION
- *   Callback function for finalizing your code. Like @initialize(),
- *   you may leave this function blank.
- */
-static void finalize(int argc, char * const argv[])
-{
+    /***********************************************************************
+     * initialize()
+     *
+     * DESCRIPTION
+     *   Call-back function for your own initialization code. It is OK to
+     *   leave blank if you don't need any initialization.
+     *
+     * RETURN VALUE
+     *   Return 0 on successful initialization.
+     *   Return other value on error, which leads the program to exit.
+     */
+    static int initialize(int argc, char* const argv[])
+    {
+        return 0;
+    }
 
-}
+
+    /***********************************************************************
+     * finalize()
+     *
+     * DESCRIPTION
+     *   Callback function for finalizing your code. Like @initialize(),
+     *   you may leave this function blank.
+     */
+    static void finalize(int argc, char* const argv[])
+    {
+        return 0;
+    }
 
 
-/*====================================================================*/
-/*          ****** DO NOT MODIFY ANYTHING BELOW THIS LINE ******      */
+    /*====================================================================*/
+    /*          ****** DO NOT MODIFY ANYTHING BELOW THIS LINE ******      */
 
-static bool __verbose = true;
-static char *__color_start = "[0;31;40m";
-static char *__color_end = "[0m";
+    static bool __verbose = true;
+    static char* __color_start = "[0;31;40m";
+    static char* __color_end = "[0m";
 
-/***********************************************************************
- * main() of this program.
- */
-int main(int argc, char * const argv[])
-{
-	char command[MAX_COMMAND_LEN] = { '\0' };
-	int ret = 0;
-	int opt;
+    /***********************************************************************
+     * main() of this program.
+     */
+    int main(int argc, char* const argv[])
+    {
+        char command[MAX_COMMAND_LEN] = { '\0' };
+        int ret = 0;
+        int opt;
 
-	while ((opt = getopt(argc, argv, "qm")) != -1) {
-		switch (opt) {
-		case 'q':
-			__verbose = false;
-			break;
-		case 'm':
-			__color_start = __color_end = "\0";
-			break;
-		}
-	}
+        while ((opt = getopt(argc, argv, "qm")) != -1) {
+            switch (opt) {
+            case 'q':
+                __verbose = false;
+                break;
+            case 'm':
+                __color_start = __color_end = "\0";
+                break;
+            }
+        }
 
-	if ((ret = initialize(argc, argv))) return EXIT_FAILURE;
+        if ((ret = initialize(argc, argv))) return EXIT_FAILURE;
 
-	if (__verbose)
-		fprintf(stderr, "%s%s%s ", __color_start, __prompt, __color_end);
+        if (__verbose)
+            fprintf(stderr, "%s%s%s ", __color_start, __prompt, __color_end);
 
-	while (fgets(command, sizeof(command), stdin)) {	
-		char *tokens[MAX_NR_TOKENS] = { NULL };
-		int nr_tokens = 0;
+        while (fgets(command, sizeof(command), stdin)) {
+            char* tokens[MAX_NR_TOKENS] = { NULL };
+            int nr_tokens = 0;
 
-		if (parse_command(command, &nr_tokens, tokens) == 0)
-			goto more; /* You may use nested if-than-else, however .. */
+            if (parse_command(command, &nr_tokens, tokens) == 0)
+                goto more; /* You may use nested if-than-else, however .. */
 
-		ret = run_command(nr_tokens, tokens);
-		if (ret == 0) {
-			break;
-		} else if (ret < 0) {
-			fprintf(stderr, "Error in run_command: %d\n", ret);
-		}
+            ret = run_command(nr_tokens, tokens);
+            if (ret == 0) {
+                break;
+            }
+            else if (ret < 0) {
+                fprintf(stderr, "Error in run_command: %d\n", ret);
+            }
 
-more:
-		if (__verbose)
-			fprintf(stderr, "%s%s%s ", __color_start, __prompt, __color_end);
-	}
+        more:
+            if (__verbose)
+                fprintf(stderr, "%s%s%s ", __color_start, __prompt, __color_end);
+        }
 
-	finalize(argc, argv);
+        finalize(argc, argv);
 
-	return EXIT_SUCCESS;
-}
+        return EXIT_SUCCESS;
+    }
